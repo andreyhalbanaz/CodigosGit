98c46def34283f51d43d98d81b8ae5adc701ea2f
Clara Kistner <clarabkistner@gmail.com>
2026-05-07 19:57:29 -0300

    Opção 1 - Novo Pedido

--- CodigoFonte/src/main/java/org/example/Main.java
+++ CodigoFonte/src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
         Scanner s = new Scanner(System.in);
 
-        double valorPago = 0;
         double valorTotalItens = 0;
 
         int[] codigosItens = {1, 2, 3, 4, 5, 6, 7};
@@ -80,63 +79,6 @@ public class Main {
                 }
 
                 System.out.println("Valor total dos itens: R$" + valorTotalItens);
-            }
-
-            if (opcaoMenu == menu[1]) {
-                System.out.println("1. Lanches");
-                System.out.println("2. Acompanhamentos");
-                System.out.println("3. Bebidas");
-                int subMenu = s.nextInt();
-
-                switch (subMenu) {
-                    case 1:
-                        System.out.println(codigosItens[0] + "- " + nomeItens[0] + " " + categoriaItens[0] + "... R$" + precoItens[0]);
-                        System.out.println(codigosItens[1] + "- " + nomeItens[1] + " " + categoriaItens[1] + "... R$" + precoItens[1]);
-                        break;
-                    case 2:
-                        System.out.println(codigosItens[2] + "- " + nomeItens[2] + " " + categoriaItens[2] + "... R$" + precoItens[2]);
-                        System.out.println(codigosItens[3] + "- " + nomeItens[3] + " " + categoriaItens[3] + "... R$" + precoItens[3]);
-                        break;
-                    case 3:
-                        System.out.println(codigosItens[4] + "- " + nomeItens[4] + " " + categoriaItens[4] + "... R$" + precoItens[4]);
-                        System.out.println(codigosItens[5] + "- " + nomeItens[5] + " " + categoriaItens[5] + "... R$" + precoItens[5]);
-                        System.out.println(codigosItens[6] + "- " + nomeItens[6] + " " + categoriaItens[6] + "... R$" + precoItens[6]);
-                        break;
-                }
-            }
-
-            if (opcaoMenu == menu[2]) {
-
-                System.out.println("Total do pedido: R$ " + valorTotalItens + "\nQual é o valor que o cliente pagou?");
-                valorPago = s.nextDouble();
-
-                while (valorPago < valorTotalItens) {
-                    System.out.println("O valor pago pelo cliente não foi o suficiente para cobrir os gastos,\n" +
-                            "insira um valor que cubra os gastos: ");
-                    valorPago = s.nextDouble();
-                }
-
-                double troco = valorPago - valorTotalItens;
-
-                System.out.println("");
-
-                System.out.printf("Troco: R$ %.2f%n", troco);
-            }
-
-            if (opcaoMenu == menu[3]) {
-                int sorteado = (int) (Math.random() * 7) + 1;
-
-                for (int item : itensPedido) {
-                    int indice = item - 1;
-                    if (sorteado == codigosItens[indice]) {
-                        valorTotalItens = (valorTotalItens - precoItens[indice])
-                                + precoItens[indice] * 0.8;
-                    }
-                }
-
-                System.out.println("O item sorteado do dia é: " + nomeItens[sorteado - 1]
-                        + " com 20% de desconto");
-                System.out.printf("O valor total dos itens com desconto é: R$ %.2f%n", valorTotalItens);
             }
 
             if (opcaoMenu == menu[4]){
